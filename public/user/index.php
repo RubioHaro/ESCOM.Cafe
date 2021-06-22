3a2840adf7b09b531e463b2b3e381e7609ec28d3
Shim <mi33sks@gmail.com>
2021-06-22 15:41:22 -0500

    correccion

--- public/user/index.php
+++ public/user/index.php
@@ -32,7 +32,7 @@
             <div class="container px-4 px-lg-5 my-5">
                 <div class="text-center text-white">
                     <h1 class="display-4 fw-bolder">ESCOM.Cafe</h1>
-                    <p class="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
+                    <p class="lead fw-normal text-white-50 mb-0">Descansate un momento con café</p>
                 </div>
             </div>
         </section>
