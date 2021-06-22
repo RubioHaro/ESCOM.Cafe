35bf7ede2a12f3e546717da71339d76f99ca43d9
Shim <mi33sks@gmail.com>
2021-06-22 16:17:57 -0500

    correccion

--- public/user/index.php
+++ public/user/index.php
@@ -115,7 +115,7 @@
     <!-- Footer-->
     <footer class="py-5 bg-dark">
         <div class="container">
-            <p class="m-0 text-center text-white">Copyright &copy; Your Website 2021</p>
+            <p class="m-0 text-center text-white">Copyright &copy; ESCOM.cafe</p>
         </div>
     </footer>
     <!-- Bootstrap core JS-->
